b4a72910fe72e0d7896111e145cf5f625ae1191e
Saikat Ghosh <g.saikat.261997@gmail.com>
2022-05-13 02:05:37 +0530

    Add files via upload

---
+++ code/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import Navigate from './compo/Navigate';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Navigate />
+    </SafeAreaView>
+  );
+};
+
+export default App;
